2f25e52ff61b23d326c9ffeb80bffc61e5947149
José Flores <josefloresgonzalez@proton.me>
2024-03-15 01:01:42 -0600

    Correccion de errores

--- JS/main.js
+++ JS/main.js
@@ -1,24 +1,22 @@
+let textoUsuario = "";
+let textoMostrar = "";
+
 function accionEnter() {
     // Se obtiene el elemento con id textoUsuario y se le agrega un evento
     document.getElementById('textoUsuario').addEventListener('keydown', function (e) {
 
         // Si la tecla presionada es "Enter"
         if (e.key === 'Enter') {
-            // Se llama a la función desencriptar
-            desencriptar();
-
             // Se previene el comportamiento por defecto del evento
             e.preventDefault();
 
-            // Dirigir a vinculo con id textoMostrar
-            document.getElementById('textoMostrar').scrollIntoView();
+            
         }
     });
 }
 
-// Definir funciones
-function desencriptar() {
-    let textoUsuario = document.getElementById('textoUsuario').value;
+function encriptar() {
+    textoUsuario = document.getElementById('textoUsuario').value;
     /*
     La letra "e" es convertida para "enter"
     La letra "i" es convertida para "imes"
@@ -27,7 +25,7 @@ function desencriptar() {
     La letra "u" es convertida para "ufat"
     */
    
-    let textoEncriptado =
+    textoMostrar =
         textoUsuario
         .replace(/[Eeé]/g, "enter")
         .replace(/[Iií]/g, "imes")
@@ -40,7 +38,40 @@ function desencriptar() {
 
     // Se muestran los elementos con clases textoSalida__mensaje
     document.querySelector('.textoSalida__mensaje').style.display = 'flex';
+
     // Se muestra el texto convertido en el elemento con id mensajeEncriptado
-    document.getElementById('mensajeEncriptado').innerHTML = textoEncriptado;
+    document.getElementById('mensajeEncriptado').innerHTML = textoMostrar;
+
+    // Dirigir a vinculo con id textoMostrar
+    document.getElementById('textoMostrar').scrollIntoView();
 }
 
+function desencriptar() {
+    textoUsuario = document.getElementById('textoUsuario').value;
+
+    textoMostrar =
+        textoUsuario
+        .replace(/enter/g, "e")
+        .replace(/imes/g, "i")
+        .replace(/ai/g, "a")
+        .replace(/ober/g, "o")
+        .replace(/ufat/g, "u")
+
+    // Se ocultan los elementos con clases textoSalidaAviso__uno y textoSalidaAviso__dos
+    document.querySelector('.textoSalida__aviso').style.display = 'none';
+
+    // Se muestran los elementos con clases textoSalida__mensaje
+    document.querySelector('.textoSalida__mensaje').style.display = 'flex';
+    
+    document.getElementById('mensajeEncriptado').innerHTML = textoMostrar;
+
+    // Dirigir a vinculo con id textoMostrar
+    document.getElementById('textoMostrar').scrollIntoView();
+}
+
+function copiar() {
+    // Al hacer clic en el botón con id btnCopiar copiar contenido de la variable textoMostrar a la papelera
+    navigator.clipboard.writeText(textoMostrar);
+    console.log('Texto copiado');
+
+}
